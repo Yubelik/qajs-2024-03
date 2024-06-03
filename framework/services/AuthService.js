5270a9b6df711554d9d31e9ac1999210ea6f611b
Yubelik <89403380+Yubelik@users.noreply.github.com>
2024-06-03 20:01:06 +0300

    Прошу проверить ДЗ

--- framework/services/AuthService.js
+++ framework/services/AuthService.js
@@ -5,13 +5,41 @@ import config from '../config/config.js'
 //   `${config.baseURL}/Account/v1/GenerateToken`,
 // )
 
+const addUser = async (userName, password) => {
+  const response = await fetch(`${config.baseURL}/Account/v1/User`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userName, password),
+  })
+  // console.log('userName = ', JSON.stringify(userName, password))
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: await response.json(),
+  }
+}
+
+const loginUser = async (userName, password) => {
+  const response = await fetch(`${config.baseURL}/Account/v1/Login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userName, password),
+  })
+  // console.log('userName = ', JSON.stringify(userName, password))
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: await response.json(),
+  }
+}
+
 const generateToken = async (userName, password) => {
   const response = await fetch(`${config.baseURL}/Account/v1/GenerateToken`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userName, password),
   })
-  console.log('userName = ', JSON.stringify(userName, password))
+  // console.log('userName = ', JSON.stringify(userName, password))
   return {
     headers: response.headers,
     status: response.status,
@@ -33,7 +61,33 @@ const authorized = async ({ userName, password }) => {
   }
 }
 
+const deletUser = async ({ uuid }) => {
+  // console.log(
+  //   'строчка с uuid для отправки = ' + `${config.baseURL}/v1/User/${uuid}`,
+  // )
+  const response = await fetch(`${config.baseURL}/v1/User/${uuid}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Basic VXNlcjpBYXNkYXNkXyEyMzEyMzE=',
+    },
+    body: 'false',
+  })
+  // console.log(response.statusText)
+  console.log(response.status)
+  return {
+    // response,
+
+    // headers: response.headers,
+    status: response.status,
+    // data: await response.json(),
+  }
+}
+
 export default {
+  addUser,
+  loginUser,
   generateToken,
   authorized,
+  deletUser,
 }
--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,5 +1,39 @@
 import AuthService from '../framework/services/AuthService.js'
 import config from '../framework/config/config.js'
+let uuidUser
+describe('Создание пользователя', () => {
+  // it('Пользователь успешно создан', async () => {
+  //   const response = await AuthService.addUser({
+  //     userName: config.userName,
+  //     password: config.password,
+  //   })
+  //   // console.log('response', response)
+  //   expect(response.status).toBe(200)
+  //   expect(response.data).toBe(true)
+  // })
+  it('Пользователь существует', async () => {
+    const response = await AuthService.addUser({
+      userName: config.userName,
+      password: config.password,
+    })
+    // console.log('response', response)
+    expect(response.status).toBe(406)
+  })
+})
+
+describe('Логин', () => {
+  it('Пользователь залогинет', async () => {
+    const response = await AuthService.loginUser({
+      userName: config.userName,
+      password: config.password,
+    })
+    expect(response.status).toBe(200)
+    // expect(response.data).toBe(true)
+    uuidUser = response.data.userId
+
+    // expect(response.data.token).toBeDefined()
+  })
+})
 
 describe('Авторизация', () => {
   it('Успешная авторизация', async () => {
@@ -7,19 +41,36 @@ describe('Авторизация', () => {
       userName: config.userName,
       password: config.password,
     })
-    console.log('response', response)
+    // console.log('response authorized', response)
     expect(response.status).toBe(200)
     expect(response.data).toBe(true)
     // expect(response.data.token).toBeDefined()
   })
 
-  //   it('Нельзя авторизоваться без пароля', async () => {
-  //     const response = await AuthService.authorized({
-  //       userName: config.userName,
-  //       password: '',
-  //     })
-  //     expect(response.status).toBe(400)
-  //     expect(response.data.code).toBe('1200')
-  //     expect(response.data.message).toBe('UserName and Password required.')
-  //   })
+  it('Нельзя авторизоваться без пароля', async () => {
+    const response = await AuthService.authorized({
+      userName: config.userName,
+      password: '',
+    })
+    expect(response.status).toBe(400)
+    expect(response.data.code).toBe('1200')
+    expect(response.data.message).toBe('UserName and Password required.')
+  })
+})
+describe('Удаление пользователя', () => {
+  it('Пользователь удален', async () => {
+    // console.log('uuidUser', uuidUser)
+    const response = await AuthService.deletUser({
+      uuid: uuidUser,
+    })
+    // console.log('response пользователь удален =', response)
+    expect(response.status).toBe(200)
+    expect(response.status).toBe(200)
+  })
+  it('Пользователь удален повторно', async () => {
+    const response = await AuthService.deletUser({
+      uuid: uuidUser,
+    })
+    expect(response.status).toBe(200)
+  })
 })
