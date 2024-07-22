cb01a5b9dd46b3c14a97e8169a8ec387a01547a7
yubelik <yubelik@mail.ru>
2024-07-22 16:21:19 +0300

    new file:   df 	new file:   e2e/autj.spec.CristalPuzzel.js 	new file:   framework/pages/AuthPage.js 	new file:   framework/pages/LoginPage.js 	new file:   framework/pages/exemple.js

---
+++ framework/pages/LoginPage.js
@@ -0,0 +1,23 @@
+export function AuthPage ({page}){
+    const visit=()=>{
+
+    }    
+    const fillUsername=()=>{
+
+    }
+
+    const fillPassword=()=>{
+    
+    }
+    const fillEmail=()=>{
+    
+    }
+
+    return{
+        visit,
+        fillUsername,
+        fillPassword,
+        fillEmail,
+
+    }
+}
