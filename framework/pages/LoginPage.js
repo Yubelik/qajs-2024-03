f4b7007962662778ad1eaa8e6232cebddbe41ccc
yubelik <yubelik@mail.ru>
2024-07-22 16:26:23 +0300

    modified:   framework/pages/LoginPage.js 	modified:   playwright.config.js

--- framework/pages/LoginPage.js
+++ framework/pages/LoginPage.js
@@ -1,6 +1,6 @@
 export function AuthPage ({page}){
-    const visit=()=>{
-
+    const visit = async () => {
+        await page.goto('https://rwa-194.87.102.103.sslip.io/register')
     }    
     const fillUsername=()=>{
 
