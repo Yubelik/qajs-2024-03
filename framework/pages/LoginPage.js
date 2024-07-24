3e4c32e1e07e6238c655a041da670ee76e645b9b
yubelik <yubelik@mail.ru>
2024-07-24 18:32:06 +0300

    Прошу проверить ДЗ

--- framework/pages/LoginPage.js
+++ framework/pages/LoginPage.js
@@ -1,23 +1,39 @@
-export function AuthPage ({page}){
+import { test, expect } from '@playwright/test'
+
+export function LoginPage ({page}){
     const visit = async () => {
-        await page.goto('https://rwa-194.87.102.103.sslip.io/register')
+        await page.goto('http://203.31.40.34/login');
+        //Войти
+        await expect(page.getByRole('button', { name: 'Войти' })).toBeVisible()
+        await expect(page).toHaveTitle("CRYSTALPUZZLES")
     }    
-    const fillUsername=()=>{
-
+    const fillUsername = async (userName) => {
+        await page.locator('input[type="email"]').fill(userName);
     }
 
-    const fillPassword=()=>{
+    const fillPassword = async (password) => {
+        await page.locator('input[type="password"]').fill(password);
+    }
+
+    const submit = async () => {
+        await page.getByLabel('Подтверждая, вы соглашаетесь на обработку персональных данных и c').check();
+        await page.getByRole('button', { name: 'Войти' }).click();
+    }
+
+    const login = async ({userName, password}) => {
+        await visit();
+        await fillUsername(userName);
+        await fillPassword(password);
+        await submit();
+    }
+
+   
     
-    }
-    const fillEmail=()=>{
-    
-    }
-
     return{
         visit,
         fillUsername,
         fillPassword,
-        fillEmail,
-
-    }
+        submit,
+        login,
+        }
 }
